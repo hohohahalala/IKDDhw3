2f768094fcd4949b7ce83fbe29ace46d176e8ebd
hohohahalala <hohohahalala1223@gmail.com>
2015-01-06 16:58:02 +0800

    IKDDhw3

--- IKDDhw3-1.py
+++ IKDDhw3-1.py
@@ -1,9 +1,21 @@
 from pyspark import SparkConf, SparkContext
+import sys,os
 
-conf = SparkConf().setAppName("IKDDhw3").setMaster("local")
-sc = SparkContext(conf=conf)
-file = sc.textFile("/Users/hohohahalala/Documents/IKDD/IKDDhw3/5000.txt.utf-8.txt")
 
-counts = file.flatMap(lambda line: line.split(" ")) \
-             .collect()
-print len(counts)
+def main():
+	conf = SparkConf().setAppName("IKDDhw3").setMaster("local")
+	sc = SparkContext(conf=conf)
+	try:
+		file_name = sys.argv[1]
+		file = sc.textFile(file_name)
+		counts = file.flatMap(lambda line: line.split(" ")) \
+				.collect()
+		print "----------------------------------------------------------"
+		print "Total word number: " + str(len(counts))
+	except:
+		print "Usage: \"./bin/pyspark python_file_path data_file_path\""
+		exit()
+
+
+if __name__ == '__main__':
+	main()
