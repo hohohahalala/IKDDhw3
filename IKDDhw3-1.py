7017006dd0549b570c1c368ba316c0731d624105
hohohahalala <hohohahalala1223@gmail.com>
2015-01-06 16:26:23 +0800

    IKDDhw3

---
+++ IKDDhw3-1.py
@@ -0,0 +1,11 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setAppName("IKDDhw3").setMaster("local")
+sc = SparkContext(conf=conf)
+file = sc.textFile("/Users/hohohahalala/Documents/IKDD/IKDDhw3/5000.txt.utf-8.txt")
+
+counts = file.flatMap(lambda line: line.split(" ")) \
+             .collect()
+print len(counts)
+# print counts.
+# counts.saveAsTextFile("/Users/hohohahalala/Documents/IKDD/IKDDhw3/count")
